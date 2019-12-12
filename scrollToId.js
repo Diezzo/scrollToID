0f2df67533f1ed72503f8c2ed61fd47353b94d5f
Ilya Konovalov <ilyaburackov@ya.ru>
2019-12-12 19:14:46 +0400

    Create scrollToId.js

---
+++ scrollToId.js
@@ -0,0 +1,14 @@
+ var links = document.querySelectorAll('.scrollTo');
+for(let i of links){
+    const elem = i.getAttribute("data-target");
+    if (elem !== '') {
+        i.addEventListener("click", handleButtonClick);
+    }
+}
+function handleButtonClick(e) {
+    const id = e.target.getAttribute("data-target");
+	console.log(id)
+    if(id){
+        document.getElementById(id).scrollIntoView({ block: "center", behavior: "smooth" });
+    }
+}
